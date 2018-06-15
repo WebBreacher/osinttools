45877b6ece2806726099ebdbb95021cca9de9722
WebBreacher <WebBreacher@users.noreply.github.com>
2018-06-15 09:59:19 -0400

    finished lab

--- aopy/twitter_account_lookup.py
+++ aopy/twitter_account_lookup.py
@@ -5,15 +5,20 @@ import requests
 import time
 
 
-#account = 'jms_dot_py'
-account = 817668451
+account = 'webbreacher'
+#account = 817668451
 
 
 #
 # Main Twitter API function for sending requests
 #
-def send_request(url, next_cursor=None):
+def send_request(account, next_cursor=None):
 
+    if str(account).isdigit():
+        url = 'https://api.twitter.com/1.1/users/show.json?user_id={}'.format(account)
+    else:
+        url = 'https://api.twitter.com/1.1/users/show.json?screen_name={}'.format(account)
+    
     response = requests.get(url, auth=oauth)
 
     time.sleep(3)
@@ -26,44 +31,10 @@ def send_request(url, next_cursor=None):
 
     return None
 
-
-
 #
 # MAIN
 #
+account_data = send_request(account)
 
-if str(account).isdigit():
-    url = 'https://api.twitter.com/1.1/users/show.json?user_id={}'.format(account)
-else:
-    url = 'https://api.twitter.com/1.1/users/show.json?screen_name={}'.format(account)
-
-account_data = send_request(url)
-
-print(account_data)
-exit()
-
-friends   = get_all_friends_followers(username, 'friends')
-followers = get_all_friends_followers(username, 'followers')
-
-print('[**] Retrieved %d friends' % len(friends))
-print('[**] Retrieved %d followers' % len(followers))
-
-snapshot_timestamp = time.time()
-
-# store the friends
-friends_file = '%s-%f-friends.txt' % (username, snapshot_timestamp)
-with open(friends_file, 'w') as fd:
-    for friend in friends:
-        fd.write('%d\n' % friend)
-
-print('[!] Stored friends in %s' % friends_file)
-
-# store the followers
-followers_file = '%s-%f-followers.txt' % (username, snapshot_timestamp)
-with open(followers_file, 'w') as fd:
-    for follower in followers:
-        fd.write('%d\n' % follower)
-
-print('[!] Stored followers in %s' % followers_file)
-
-
+print('ID, Name, Screen Name')
+print('{}, {}, {}'.format(account_data['id'], account_data['name'], account_data['screen_name']))
