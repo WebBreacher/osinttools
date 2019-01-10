519c1b7d61f8bfdafa43075b8258958944c949c0
WebBreacher <WebBreacher@users.noreply.github.com>
2019-01-10 12:33:34 -0500

    renaming tool. added apiv2 support

--- hibp_requestor.py
+++ hibp_requestor.py
@@ -10,7 +10,7 @@ import requests
 import sys
 import time
 
-#Disables SSL warning
+# Disables SSL warning
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
@@ -33,18 +33,20 @@ for email in read_data:
     # Check to see if the input has an "@" symbol. If not, skip it.
     if '@' not in email:
         continue
-    url = 'https://haveibeenpwned.com/api/breachedaccount/' + email.strip()
-    r = requests.get(url)
+    url = 'https://haveibeenpwned.com/api/v2/breachedaccount/' + email.strip()
+    headers = {'User-Agent': 'SANS SEC487 Checker'}
+    r = requests.get(url, headers=headers, verify=False)
     print(email.strip())  
     
     # If the server sends a 200 response code then there is data
     if (r.status_code == 200):
         # Iterate through the list of the results and print the Name of the breach
         for site in r.json():
-           print ('  !! Found in the breach: ' + site)
+           print ('  !! Found in the breach: {0}, Breach Date: {1}'.format(site['Title'], site['BreachDate']))
     else:
         # Anything other than a response code of 200 and we assume no breach
         print('    Not breached')
     
+    print ()
     # Per HIBP API rules, we need to rate limit > 1800ms
     time.sleep(2)
